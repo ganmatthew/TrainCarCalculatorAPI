65170baa349d50c87f0e1e043ed16cebd9d81b84
ganmatthew <ganmatthew09@gmail.com>
2026-06-04 23:38:43 +0800

    Fixed data retrieval and validation

---
+++ src/get_line_data.ts
@@ -0,0 +1,15 @@
+import LRT1 from '../data/LRT1.json';
+import LRT2 from '../data/LRT2.json';
+import MRT3 from '../data/MRT3.json';
+
+const trainDataMap: Record<string, any> = {
+    "LRT1": LRT1,
+    "LRT2": LRT2,
+    "MRT3": MRT3
+};
+
+function getLineData(line: string) {
+    return trainDataMap[line]
+}
+
+export { getLineData }
